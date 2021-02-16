0116dcbfe1ca9dd48f73a808caf39828db8cdaae
jackliasi <77193943+jackliasi@users.noreply.github.com>
2021-02-16 15:23:33 +0000

    Add files via upload

---
+++ slingshot.js
@@ -0,0 +1,16 @@
+class SlingShot{
+    constructor(body1,body2){
+        var options ={
+            bodyA:body1,
+            bodyB:body2,
+            length:10,stiffness:0.6,
+        }
+       this.sling=Matter.Constraint.create(options);
+        World.add(world,this.sling)
+    }
+    display(){
+        var pointA=this.sling.bodyA.position
+        var pointB=this.sling.bodyB.position
+        line (pointA.x,pointA.y,pointB.x,pointB.y)
+    }
+}
